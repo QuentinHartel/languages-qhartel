74e8a8bbec7d303e13501db36f42de9ee5e1a078
QuentinHartel <112497036+QuentinHartel@users.noreply.github.com>
2024-04-22 16:01:49 +0000

    Reg Expressions

--- RegExpressions/spec/num_spec.rb
+++ RegExpressions/spec/num_spec.rb
@@ -1,22 +1,26 @@
 # regex_spec.rb
+require 'rspec'
+
 RSpec.describe 'Regular Expression for decimal literal' do
-  let(:nonzero_decimal_digit) { /[1-9]/ }
-  let(:decimal_digit) { /[0-9]/ }
+  
+  let(:signOpt) {/-?/}
+  let(:digit) {/\d+/}
+  let(:thousandSep) {/(?:'\d{3})*/}
+  let(:size) {/[uU]?[lL]{0,2}/}
+  
+  let(:regex) {/^#{signOpt}#{digit}#{thousandSep}#{size}$/}
+
   let(:octal_digit) {  /[0-7]/ }
   let(:hex_digit) {  /[0-9]|[A-F]|[a-f]/ }
   let(:binary_digit) {  /[0-1]/ }
-  let(:decimal) { /#{nonzero_decimal_digit}('?#{decimal_digit}+)*/ }
-  let(:size) { /[uU]?[lL]{0,2}/ }
-
-  let(:pattern) { /^-?#{decimal}#{size}$/ }
-
-  let(:should_pass) { [ "1", "-33'000", "4525235", "10'080", "123'456'789", "1ul", "1u", "1ll" ] }
-  let(:should_fail) { ["'1'", "1'''3", "afed", "+33", "0", "ul", "lll", "3lll", "3uuull" ] }
+  
+  let(:should_pass) { [ "1", "-33'000", "4525235", "10'080", "123'456'789", "1ul", "1u", "1ll", "0"] }
+  let(:should_fail) { ["'1'", "1'''3", "afed", "+33", "ul", "lll", "3lll", "3uuull"] }
 
   describe 'should pass' do
     it 'matches all expected strings' do
       should_pass.each do |str|
-        expect(str).to match(pattern)
+        expect(str).to match(regex)
       end
     end
   end
@@ -24,8 +28,13 @@ RSpec.describe 'Regular Expression for decimal literal' do
   describe 'should fail' do
     it 'does not match any of the strings' do
       should_fail.each do |str|
-        expect(str).not_to match(pattern)
+        expect(str).not_to match(regex)
       end
     end
   end
 end
+
+RSpec::Core::Runner.run([])
+
+# To check the results programmatically
+puts "RSpec results: #{RSpec.world.filtered_examples.values.flatten.map(&:execution_result).map(&:status)}"
